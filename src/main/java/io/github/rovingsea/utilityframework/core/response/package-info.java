9dc1ab03150bbd88879c185035b0ab997cf16267
RovingSea <1262917629@qq.com>
2022-09-29 22:41:26 +0800

    refactor(Project Structure): Removing parent module code

--- src/main/java/io/github/rovingsea/utilityframework/core/response/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This module allows developers to define unified response content,
- * but it is currently implemented through {@link io.github.rovingsea.utilityframework.core.response.Response}.
- * In the future, it is hoped that programmers can complete it through configuration
- */
-package io.github.rovingsea.utilityframework.core.response;
--- src/main/java/io/github/rovingsea/utilityframework/core/service/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * TODO This module, I want to be able to extract some commonalities of 'Service'
- *      and derive a tool class or template
- */
-package io.github.rovingsea.utilityframework.core.service;
