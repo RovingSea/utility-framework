0013c6604f73c00147ab01bd2ecdd21e1f046fff
RovingSea <1262917629@qq.com>
2022-10-09 17:09:56 +0800

    docs(#5): Improve javadoc

--- utility-spring-boot-core/src/main/java/io/github/rovingsea/utilityframework/core/exception/HandlingExceptionResult.java
+++ utility-spring-boot-core/src/main/java/io/github/rovingsea/utilityframework/core/exception/HandlingExceptionResult.java
@@ -1,10 +1,18 @@
 package io.github.rovingsea.utilityframework.core.exception;
 
+import io.github.rovingsea.utilityframework.core.response.ControllerResponseAdvice;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * <p>
+ * It is used to distinguish between normal results and exception results in
+ * {@link ControllerResponseAdvice}. If it is an exception result, return the
+ * result according to other logic.
+ * </p>
+ *
  * @author Haixin Wu
  * @since 1.0.0
  */
